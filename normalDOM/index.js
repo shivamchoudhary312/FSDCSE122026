e22c17c7bb836bf9a3589ca658a1c9f84936e695
shivamchoudhary312 <shivam1122choudhary@gmail.com>
2026-08-24 10:15:17 +0530

    border color

--- normalDOM/index.js
+++ normalDOM/index.js
@@ -24,6 +24,7 @@ function buildresume() {
         
         const table = document.createElement('table');
         table.border = '1';
+        table.style.backgroundColor = 'black';
 
     
         const row1 = document.createElement('tr');
